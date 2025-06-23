4adde601b82621ed6477e1413ac99db753cf59b1
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-06-23 18:58:39 +0530

    feat: add Stream API integration with user upsert functionality

---
+++ backend/src/db/stream.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import { StreamChat } from "stream-chat";
+
+dotenv.config({
+    path : './.env'
+});
+
+const apiKey = process.env.STEAM_API_KEY;
+const apiSecret = process.env.STEAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    console.error(`Stream API key or secret is missing`);
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        if (!streamClient) {
+            throw new Error("Stream client not initialized.");
+        }
+        await streamClient.upsertUsers([userData]);
+        return userData;
+    } catch (error) {
+        console.error("Error upserting Stream user ", error);
+        throw error;
+    }
+};
+
