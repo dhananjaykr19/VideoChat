13c09ce2633b4994b472b165b0f13dc7df8bf26f
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-06-25 00:48:19 +0530

    feat: add routing and pages for Home, SignUp, Login, Notification, Call, Chat, and Onboarding; integrate react-hot-toast and react-query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Route, Routes } from 'react-router';
+
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import NotificationPage from './pages/NotificationPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import OnboardingPage from './pages/OnboardingPage.jsx';
+
+import toast, { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
-    <div className="bg-red-500 h-screen">App</div>
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route path="/" element = {<HomePage/>} />
+        <Route path="/signup" element = {<SignUpPage />} />
+        <Route path="/login" element = { <LoginPage /> } />
+        <Route path="/notification" element = { <NotificationPage /> } />
+        <Route path="/call" element = { <CallPage /> } />
+        <Route path="/chat" element = { <ChatPage /> } />
+        <Route path="/onboarding" element = { <OnboardingPage /> } />
+      </Routes>
+      <Toaster />
+    </div>
   )
 }
 
