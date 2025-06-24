8262af04b9e9b3528c3ba24f5ef732f6b975e009
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-06-24 14:35:34 +0530

    feat: enhance getMyFriends controller with error handling and response structure

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -24,5 +24,16 @@ export async function getRecommendedUsers(req, res) {
 }
 
 export async function getMyFriends(req, res) {
-    
+    try {
+        const user = await User.findById(req.user._id)
+        .select("friends")
+        .populate("friends", "fullName profilePic nativeLanguage learningLanguage location");
+        
+        res.status(200).json(user.friends);
+    } catch (error) {
+        console.log("Error in getMyFriends controller : ", error.message);
+        res.status(500).json({
+            message : "Internal server error"
+        });
+    }
 }
