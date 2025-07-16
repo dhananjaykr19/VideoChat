a9f57924cfab2e7d62e994a5e4c77c89e9a2cd2b
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-07-17 00:24:14 +0530

    feat: implement useLogout hook for user logout functionality and update API for logout endpoint

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,16 @@
+import { useLocation } from "react-router";
+import useAuthUser from "../hooks/useAuthUser"
 
 const Navbar = () => {
+    const { authUser } = useAuthUser();
+    const location = useLocation();
+    const isChatPage = location.pathname?.startsWith("/chat");
+    
+
     return (
-        <div>Navbar</div>
+        <nav>
+            
+        </nav>
     )
 }
 
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -8,14 +8,19 @@ export const signup = async (signupData) => {
 export const login = async (loginData) => {
   const response = await axiosInstance.post("/auth/login", loginData);
   return response.data;
-}
+};
+
+export const logout = async () => {
+  const response = await axiosInstance.post("/auth/logout");
+  return response.data;
+};
 
 export const getAuthUser = async () => {
   const res = await axiosInstance.get("/auth/me");
   return res.data;
-}
+};
 
 export const completeOnboarding = async (userData) => {
   const response = await axiosInstance.post("/auth/onboarding", userData);
   return response.data;
-}
+};
