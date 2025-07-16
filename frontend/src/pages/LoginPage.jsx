f21cf04917fc47049b1fcbb190b84ab8d860d196
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-07-16 08:53:56 +0530

    feat: implement custom useLogin hook for handling user authentication in LoginPage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,7 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { ShipWheelIcon } from "lucide-react";
 import { useState } from "react"
-import { login } from "../lib/api";
 import { Link } from "react-router";
+import useLogin from "../hooks/useLogin";
 
 const LoginPage = () => {
     const [loginData, setLoginData] = useState({
@@ -10,14 +9,7 @@ const LoginPage = () => {
         password : ""
     });
 
-    const queryClient = useQueryClient();
-
-    const {mutate : loginMutation, isPending, error} = useMutation({
-        mutationFn : login,
-        onSuccess : () => queryClient.invalidateQueries({
-            queryKey : ["authUser"]
-        }),
-    });
+    const {error, isPending, loginMutation} = useLogin();
 
     const handleLogin = (e) => {
         e.preventDefault();
