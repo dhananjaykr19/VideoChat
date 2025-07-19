4075c34c39fc928bb00b8b1b2b7a5558ef7211ca
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-07-19 11:49:20 +0530

    feat: implement friend request handling in NotificationPage

--- frontend/src/pages/NotificationPage.jsx
+++ frontend/src/pages/NotificationPage.jsx
@@ -1,4 +1,29 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { acceptFriendRequest, getFriendRequests } from "../lib/api";
+
 const NotificationPage = () => {
+    const queryClient = useQueryClient();
+
+    const { data : friendRequests, isLoading } = useQuery({
+        queryKey : ["friendRequests"],
+        queryFn : getFriendRequests,
+    });
+
+    const { mutate : acceptRequestMutation, isPending } = useMutation({
+        mutationFn : acceptFriendRequest,
+        onSuccess : () => {
+            queryClient.invalidateQueries({
+                queryKey : ["friendRequests"]
+            });
+            queryClient.invalidateQueries({
+                queryKey : ["friends"]
+            });
+        },
+    });
+
+    const incomingRequests = friendRequests?.incomingReqs || [];
+    const acceptedRequests = friendRequests?.acceptRequest || [];
+    
     return (
         <div>NotificationPage</div>
     )
