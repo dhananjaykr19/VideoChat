44cc1bdb82c238eaeef87df0678ba35dac41b3d4
dhananjaykr19 <iamdhananjay1929@gmail.com>
2025-07-19 14:12:26 +0530

    Fixing Chat routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,7 +51,15 @@ const App = () => {
           path="/call" element = {isAuthenticated ? <CallPage /> : <Navigate to="/login" /> } 
         />
         <Route 
-          path="/chat" element = {isAuthenticated ? <ChatPage /> : <Navigate to="/login" /> } 
+          path="/chat/:id" element = {
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={true}>
+                <ChatPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
         />
         <Route 
           path="/onboarding" element = {isAuthenticated ? ( isOnboarded ? (<OnboardingPage /> ):( <Navigate to="/" /> ) ) : (
